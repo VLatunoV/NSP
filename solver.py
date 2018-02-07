d9fbb217106e6037265d5433aaccada5c30957b3
Valentin Latunov <vlatunov@gmail.com>
2018-02-07 15:47:04 +0200

    An optimal solution for instance 1

--- solver.py
+++ solver.py
@@ -6,7 +6,21 @@ class SolutionInstance:
 	'''
 	def __init__(self):
 		self.horizon = 0
+		self.score = 0
 		self.schedule = dict()
+
+# Instance 1
+exact_solution = SolutionInstance()
+exact_solution.horizon = 14
+exact_solution.schedule['A'] = [' ', 'D', 'D', 'D', 'D', ' ', ' ', 'D', 'D', ' ', ' ', 'D', 'D', 'D']
+exact_solution.schedule['B'] = ['D', 'D', 'D', 'D', 'D', ' ', ' ', 'D', 'D', ' ', ' ', 'D', 'D', ' ']
+exact_solution.schedule['C'] = ['D', 'D', 'D', ' ', ' ', 'D', 'D', ' ', ' ', 'D', 'D', 'D', ' ', ' ']
+exact_solution.schedule['D'] = ['D', 'D', ' ', ' ', ' ', 'D', 'D', 'D', 'D', 'D', ' ', ' ', ' ', ' ']
+exact_solution.schedule['E'] = [' ', 'D', 'D', 'D', 'D', ' ', ' ', 'D', 'D', ' ', ' ', 'D', 'D', 'D']
+exact_solution.schedule['F'] = ['D', 'D', 'D', 'D', 'D', ' ', ' ', 'D', 'D', ' ', ' ', ' ', 'D', 'D']
+exact_solution.schedule['G'] = [' ', ' ', 'D', 'D', 'D', ' ', ' ', 'D', 'D', ' ', ' ', 'D', 'D', 'D']
+exact_solution.schedule['H'] = ['D', 'D', ' ', ' ', ' ', ' ', ' ', ' ', 'D', 'D', 'D', 'D', 'D', ' ']
+exact_solution.optimal_score = 607
 
 '''
 Simulated Annealing has 4 major parts:
