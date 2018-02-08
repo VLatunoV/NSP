e988bba1437401ba2050607e95337280d96f5a32
Martin Georgiev <martin.georgiev@abbaty.int>
2018-02-08 08:58:27 +0200

    added missing checks for constraints in validator and tested

--- validator.py
+++ validator.py
@@ -9,16 +9,20 @@ class StaffMemberResult:
         self.weekends = 0
         self.prohibitedShiftChecked = True
         self.maxShiftChecked = True
+        self.daysOffChecked = True
         
     def build_info(self, solution, problem, id):
         self.id = id
-        member_shifts = solution[id];
+        member_shifts = solution[id]
         curr_consecutive_shifts = 0
         curr_consecutive_daysoff = 0
         last_shift = ''
         for idx, shift in enumerate(member_shifts):           
             if shift != ' ':
-                if (problem.shifts[last_shift] and shift in problem.shifts[last_shift].prohibitNext):
+                if (idx in problem.staff[id].daysOff):
+                    self.daysOffChecked = False
+                
+                if (last_shift in problem.shifts and shift in problem.shifts[last_shift].prohibitNext):
                     self.prohibitedShiftChecked = False
                 last_shift = shift
                 
@@ -44,8 +48,8 @@ class StaffMemberResult:
         if self.consecutiveShifts < curr_consecutive_shifts:
                     self.consecutiveShifts = curr_consecutive_shifts
         
-        for key in problem.staff[id]:
-            if problem.staff[id].maxShifts[key] < len([x for x in member_shifts if x == key]):
+        for key in problem.staff:
+            if key in problem.staff[id].maxShifts and problem.staff[id].maxShifts[key] < len([x for x in member_shifts if x == key]):
                 self.maxShiftChecked = False
         
     def calc_penalty(self, solution, problem):
@@ -54,28 +58,28 @@ class StaffMemberResult:
     def is_valid(self, problem):
         return self.maxShiftChecked and \
                 self.prohibitedShiftChecked and \
-                self.totalMinutes >= problem.staff[member].minTotalMinutes and \
-                self.totalMinutes <= problem.staff[member].maxTotalMinutes and \
-                self.consecutiveShifts >= problem.staff[member].minConsecutiveShifts and \
-                self.consecutiveShifts <= problem.staff[member].maxConsecutiveShifts and \
-                self.consecutiveDaysOff >= problem.staff[member].minConsecutiveDaysOff and \
-                self.weekends <= problem.staff[member].maxWeekends
+                self.totalMinutes >= problem.staff[self.id].minTotalMinutes and \
+                self.totalMinutes <= problem.staff[self.id].maxTotalMinutes and \
+                self.consecutiveShifts >= problem.staff[self.id].minConsecutiveShifts and \
+                self.consecutiveShifts <= problem.staff[self.id].maxConsecutiveShifts and \
+                self.consecutiveDaysOff >= problem.staff[self.id].minConsecutiveDaysOff and \
+                self.weekends <= problem.staff[self.id].maxWeekends
 # end class
 
 def ValidateSolution(solution, problem):
     is_valid = True
     for staff in problem.staff:
         staff_res = StaffMemberResult()
-        staff_res.build(solution, problem, staff)
+        staff_res.build_info(solution.schedule, problem, staff)
         is_valid = is_valid and staff_res.is_valid(problem)
         if (not is_valid):
             break
-    return is_valid
+        return is_valid
             
 def calculatePenalty(solution, problem):
     sum_amm = 0
     for staff in problem.staff:
         staff_res = StaffMemberResult()
-        staff_res.build(solution, problem, staff)
+        staff_res.build_info(solution, problem, staff)
         sum_amm += staff_res.calc_penalty(solution, problem)
-    return sum_amm;
+    return sum_amm
