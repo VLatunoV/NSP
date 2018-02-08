1f3220bf0227082acde26628d0e555e69148b59a
Martin Georgiev <martingeo95@gmail.com>
2018-02-08 11:15:03 +0200

    added penalty calculation in validator

--- validator.py
+++ validator.py
@@ -10,6 +10,8 @@ class StaffMemberResult:
         self.prohibitedShiftChecked = True
         self.maxShiftChecked = True
         self.daysOffChecked = True
+        self.offRequestPenalty = 0
+        self.onRequestPenalty = 0
         
     def build_info(self, solution, problem, id):
         self.id = id
@@ -19,6 +21,9 @@ class StaffMemberResult:
         last_shift = ''
         for idx, shift in enumerate(member_shifts):           
             if shift != ' ':
+                if (idx in problem.staff[id].shiftOffRequests):
+                    self.offRequestPenalty += problem.staff[id].shiftOffRequests[idx].weight
+                
                 if (idx in problem.staff[id].daysOff):
                     self.daysOffChecked = False
                 
@@ -35,6 +40,9 @@ class StaffMemberResult:
                     self.consecutiveDaysOff = curr_consecutive_daysoff
                 curr_consecutive_daysoff = 0
             else:
+                if (idx in problem.staff[id].shiftOnRequests):
+                    self.onRequestPenalty += problem.staff[id].shiftOnRequests[idx].weight
+                
                 if (idx + 1) % 7 == 0 and member_shifts[idx] != ' ': 
                     self.weekends += 1
                 
@@ -52,8 +60,9 @@ class StaffMemberResult:
             if key in problem.staff[id].maxShifts and problem.staff[id].maxShifts[key] < len([x for x in member_shifts if x == key]):
                 self.maxShiftChecked = False
         
-    def calc_penalty(self, solution, problem):
-        print('Not implemented')
+    def calc_penalty(self):
+        return self.offRequestPenalty + self.onRequestPenalty
+        
         
     def is_valid(self, problem):
         return self.maxShiftChecked and \
@@ -66,7 +75,7 @@ class StaffMemberResult:
                 self.weekends <= problem.staff[self.id].maxWeekends
 # end class
 
-def ValidateSolution(solution, problem):
+def validateSolution(solution, problem):
     is_valid = True
     for staff in problem.staff:
         staff_res = StaffMemberResult()
@@ -77,9 +86,19 @@ def ValidateSolution(solution, problem):
         return is_valid
             
 def calculatePenalty(solution, problem):
-    sum_amm = 0
+    staff_penalty = 0
     for staff in problem.staff:
         staff_res = StaffMemberResult()
-        staff_res.build_info(solution, problem, staff)
-        sum_amm += staff_res.calc_penalty(solution, problem)
-    return sum_amm
+        staff_res.build_info(solution.schedule, problem, staff)
+        staff_penalty += staff_res.calc_penalty()
+    cover_penalty = 0
+    for day in range(problem.horizon):
+        for shift in problem.shifts:
+            for cover in [cover[shift] for cover in problem.cover if cover[shift].day == day]:
+                schedule_day = [solution.schedule[key][day] for key in solution.schedule]
+                diff = cover.requirement - len([x for x in schedule_day if x == cover.shiftId])
+                if diff < 0:
+                    cover_penalty += (-diff) * cover.weightForOver 
+                else:
+                    cover_penalty += diff* cover.weightForUnder
+    return staff_penalty + cover_penalty
